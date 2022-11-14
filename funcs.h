5d5ec8261df96c812889d1a526e94feba5ffd85c
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-11-13 19:49:28 -0500

    added initializations for all functions

---
+++ funcs.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include "time.h"
+#include "movie.h"
+#include "timeslot.h"
+
+int minutesSinceMidnight(Time time);
+int minutesUntil(Time earlier, Time later);
+Time addMinutes(Time time0, int min);
+std::string getTimeSlot(TimeSlot timeslot);
+TimeSlot scheduleAfter(TimeSlot ts, Movie nextMovie);
+bool timeOverlap(TimeSlot ts1, TimeSlot ts2);
