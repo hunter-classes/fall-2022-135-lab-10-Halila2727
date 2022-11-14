b1c1f0971c6db9abde8ef65d24593b27d8bb6359
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-11-13 19:50:28 -0500

    called all classes and set up main function

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+  Author: Halil Akca
+  Course: CSCI-135
+  Instructor: Mike Zamansky
+  Assignment: Lab 10
+  This program will call the functions made in other .cpp files
+*/
+
+#include <iostream>
+#include "funcs.h"
+#include "time.h"
+#include "movie.h"
+#include "timeslot.h"
+
+
+int main()
+{
+    return 0;
+}
