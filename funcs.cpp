eab1fd82b696a0fac5854792cc4a42a1e0ddf1ca
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-11-17 15:18:20 -0500

    A and B task functions now work

--- funcs.cpp
+++ funcs.cpp
@@ -17,9 +17,55 @@ void printTime(Time time)
     std::cout << time.h << ":" << time.m;
 }
 
-int minutesSinceMidnight(Time time);
-int minutesUntil(Time earlier, Time later);
-Time addMinutes(Time time0, int min);
-std::string getTimeSlot(TimeSlot timeslot);
-TimeSlot scheduleAfter(TimeSlot ts, Movie nextMovie);
-bool timeOverlap(TimeSlot ts1, TimeSlot ts2);
+int minutesSinceMidnight(Time time)
+{
+    return time.h * 60 + time.m;
+}
+
+int minutesUntil(Time earlier, Time later)
+{
+    int timeSince = later.h - earlier.h;
+    timeSince *= 60;
+    if(later.m > earlier.m)
+    {
+        timeSince += later.m - earlier.m;
+    }
+    else
+    {
+        int newLater = later.m + 60;
+        timeSince -= 60;
+        timeSince += later.m - earlier.m;
+    }
+    return timeSince;
+}
+
+Time addMinutes(Time time0, int min)
+{
+    Time timeFin;
+    timeFin.m = time0.m + min;
+    int hrs = 0;
+    
+    while(timeFin.m > 59)
+    {
+        timeFin.m -= 60;
+        hrs++;
+    }
+
+    timeFin.h = time0.h + hrs;
+    return timeFin;
+}
+
+std::string getTimeSlot(TimeSlot timeslot)
+{
+
+}
+
+TimeSlot scheduleAfter(TimeSlot ts, Movie nextMovie)
+{
+
+}
+
+bool timeOverlap(TimeSlot ts1, TimeSlot ts2)
+{
+    
+}
