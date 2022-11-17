937ac66a2894f5143e033a1872e2c791aee5d6cc
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-11-17 15:19:00 -0500

    added outputs for task A and B functions

--- main.cpp
+++ main.cpp
@@ -15,5 +15,18 @@
 
 int main()
 {
-    return 0;
+  Time time1 = {10, 30};
+  Time time2 = {13, 40};
+  Time time3 = {8,10};
+  std::cout << "\nTask A: Time Since Midnight:\nTime Right Now: 10:30\nMinutes Since Midnight: " << minutesSinceMidnight(time1) << "\n";
+  std::cout << "\n------------------\n";
+
+  std::cout << "\nTask A: Minutes Until:\nEarlier Time: 10:30\nLater Time: 13:40\nMinutes between: " << minutesUntil(time1, time2) << "\n";
+  std::cout << "\n------------------\n";
+
+  std::cout << "\nTask B: Add Minutes:\nTime: 8:10\nMinutes to add: 75\n";
+  printTime(addMinutes(time3, 75));
+  std::cout << "\n\n------------------\n";
+
+  return 0;
 }
